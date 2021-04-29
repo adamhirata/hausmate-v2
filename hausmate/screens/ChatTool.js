de9d1211f1eb4cec8d67cbd576700e398c14299b
Ray Law <rlaw5@lion.lmu.edu>
2021-04-28 17:38:46 -0700

    changing images

--- hausmate/screens/ChatTool.js
+++ hausmate/screens/ChatTool.js
@@ -19,11 +19,15 @@ function ChatMessage(props) {
   return (
     <>
       <div className={`message ${messageClass}`}>
-        <img
-          src={
-            "https://cnet1.cbsistatic.com/img/hexvS3BuwnRL8ecpa19ZqsHZTWY=/1200x675/2021/02/09/62a82602-5227-4b0c-875c-302af79cc49e/zoomcat.jpg"
-          }
-        />
+        {
+          <img
+            src={
+              className === sent
+                ? "https://scontent-lax3-1.cdninstagram.com/v/t51.2885-19/s150x150/118577194_645907619663595_8572174081709089933_n.jpg?tp=1&_nc_ht=scontent-lax3-1.cdninstagram.com&_nc_ohc=JlLiuXS_Mg0AX8sLGVF&edm=ABfd0MgBAAAA&ccb=7-4&oh=1c53dcc2200ed30a2aeec8c2b6c91f1e&oe=60ADC5E7&_nc_sid=7bff83"
+                : "https://scontent-lax3-1.xx.fbcdn.net/v/t1.6435-9/44452996_333100514132084_7206387791867936768_n.jpg?_nc_cat=102&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=2PxJMGa9FA4AX_DYHlS&_nc_ht=scontent-lax3-1.xx&oh=582d30a89cd3162e34cb9389f219d23b&oe=60B06BA2"
+            }
+          />
+        }
         <p>{text}</p>
       </div>
     </>
